0a341b23de79f9daddf4d80d1a663b889c96c1e9
alexandergreif <133988732+alexandergreif@users.noreply.github.com>
2025-01-28 12:57:53 +0100

    Add files via upload

---
+++ game_function.py
@@ -0,0 +1,16 @@
+def game(num_rounds, username1, username2):
+    global score_board
+    score_board = {}
+    score_board[username1] = 20
+    score_board[username2] = 100
+    #for num in range(1, num_round + 1):
+        #game_round(num)
+
+    if score_board[username1] > score_board[username2]:
+        print(f"The winner is {username1} with {score_board[username1]} points. Congratulations!!!")
+        print(f"{username2} scored {score_board[username2]} points. GG")
+    else:
+        print(f"The winner is {username2} with {score_board[username2]} points. Congratulations!!!")
+        print(f"{username1} scored {score_board[username1]} points. GG")
+
+
